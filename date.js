2e5b828ed81e52bcf85fb98c90b246c83c584aa0
Khaled Hamdy <khaledhamdy609@gmail.com>
2023-08-19 08:45:46 +0300

    modifing the code

--- date.js
+++ date.js
@@ -1,15 +1,14 @@
-function getDate() {
-  // get html element
-  const dateElement = document.querySelector(".todo__date");
-  const timeElement = document.querySelector(".todo__time");
+const dateElement = document.querySelector(".todo__date");
+const timeElement = document.querySelector(".todo__time");
 
-  const currentDate = new Date();
+function getDate() {
+  const [getDay, getFullYear] = [
+    new Date().getDate(),
+    new Date().getFullYear(),
+  ];
   // Get various components of the current date and time
-  const year = currentDate.getFullYear();
-  const month = currentDate.getMonth() + 1; // Months are zero-based, so add 1
-  const day = currentDate.getDate();
-  const monthName = currentDate.toLocaleString("en-US", { month: "long" });
-  const time = currentDate.toLocaleString("en-US", {
+  const monthName = new Date().toLocaleString("en-US", { month: "long" });
+  const time = new Date().toLocaleString("en-US", {
     hour: "numeric",
     minute: "numeric",
     second: "numeric",
@@ -17,11 +16,12 @@ function getDate() {
   });
 
   dateElement.innerHTML = `
-    <sup class="todo__date-day">${day}</sup>
+    <sup class="todo__date-day">${getDay}</sup>
     <span class="todo__date-month">${monthName}</span>
-    <sub class="todo__date-year">${year}</sub>
+    <sub class="todo__date-year">${getFullYear}</sub>
   `;
   timeElement.innerHTML = `${time}`;
 }
 window.addEventListener("load", getDate);
+
 setInterval(getDate, 1000);
--- main.js
+++ main.js
@@ -5,34 +5,31 @@ const todoCategoryText = document.querySelector(".todos__title span");
 
 let todoCategoryStatus = sessionStorage.getItem("todoCategoryStatus") || "all";
 
-window.onload = () => {
-  renderTodoList();
-};
+window.onload = renderTodoList;
 
 function renderTodoList() {
   let todoListHTML = "";
-
   todonum.innerHTML = `Items left: ${todoList.length}`;
-
   todoCategoryText.innerHTML = `<span>${todoCategoryStatus}</span>`;
 
-  todoList.forEach((todo, index) => {
-    if (todoCategoryStatus !== "all" && todo.status !== todoCategoryStatus) return;
+  todoList.forEach(({ status, time, text }, index) => {
+    if (todoCategoryStatus !== "all" && status !== todoCategoryStatus) return;
+
     todoListHTML += `
     <div class="todos__item">
       <div class="todos__item-clock">
         <i>🕰️</i>
-        <span>${todo.time}</span>
+        <span>${time}</span>
       </div>
       <div class="todos__item__content">
         <div
         name="status"
-        class="todos__item-status ${todo.status}"
+        class="todos__item-status ${status}"
         onclick="changeTodoStatus(${index})"
         >
           <i class="fa-solid fa-check"></i>
         </div>
-        <p class="todos__item-text" id="todo-text${index}">${todo.text}</p>
+        <p class="todos__item-text" id="todo-text${index}">${text}</p>
         <div class="todos__item-btns">
           <button class="todos__item-btn__edit" onclick="ModifyElement(${index})"">🖊️</button>
           <button class="todos__item-btn__del" onclick="deleteElement(${index})">X</button>
@@ -45,34 +42,31 @@ function renderTodoList() {
 }
 
 const todoForm = document.querySelector(".todo__form");
-
+const todoInput = document.querySelector(".todo__form-input");
 todoForm.addEventListener("submit", (e) => {
   e.preventDefault();
-  const todoInput = document.querySelector(".todo__form-input");
-  const todoText = todoInput.value;
-  if (!todoText) {
-    alert("Please enter a todo");
-  } else {
-    const todo = {
-      text: todoText,
-      status: "active",
-      time: new Date().toLocaleString("en-US", {
-        hour: "numeric",
-        minute: "numeric",
-        second: "numeric",
-        hour12: true,
-      }),
-    };
-    todoList.push(todo);
-    localStorage.setItem("todoList", JSON.stringify(todoList));
-    todoInput.value = "";
-    renderTodoList();
-  }
+  const { value } = todoInput;
+  if (!value) return alert("Please enter a todo");
+
+  todoList.push({
+    text: value,
+    status: "active",
+    time: new Date().toLocaleString("en-US", {
+      hour: "numeric",
+      minute: "numeric",
+      second: "numeric",
+      hour12: true,
+    }),
+  });
+  localStorage.setItem("todoList", JSON.stringify(todoList));
+  todoInput.value = "";
+  renderTodoList();
 });
 
 function deleteElement(index) {
   let check = confirm("Are you sure you want to delete this todo?");
   if (!check) return;
+
   todoList.splice(index, 1);
   localStorage.setItem("todoList", JSON.stringify(todoList));
   renderTodoList();
@@ -80,21 +74,17 @@ function deleteElement(index) {
 
 function ModifyElement(index) {
   const todoText = prompt("Enter new todo", todoList[index].text);
-  if (!todoText) {
-    alert("Please enter a todo");
-  } else {
-    todoList[index].text = todoText;
-    localStorage.setItem("todoList", JSON.stringify(todoList));
-    renderTodoList();
-  }
+  if (!todoText) return alert("Please enter a todo");
+
+  todoList[index].text = todoText;
+  localStorage.setItem("todoList", JSON.stringify(todoList));
+  renderTodoList();
 }
 
 function changeTodoStatus(index) {
-  if (todoList[index].status === "active") {
-    todoList[index].status = "completed";
-  } else {
-    todoList[index].status = "active";
-  }
+  todoList[index].status =
+    todoList[index].status === "active" ? "completed" : "active";
+
   localStorage.setItem("todoList", JSON.stringify(todoList));
   renderTodoList();
 }
